4f98ef556cbb02c0af72c6c1cd98886a85caddbd
sato1043 <sato1043@updater.cc>
2020-01-12 17:25:07 +0900

    feat(background.js): 現在タブを新ウィンドウに移動するのみに改変
    ## 2020/1/12 sato1043改変版追記
    - 複数タブのcombine/splitを無効化
    - 機能拡張メニューのボタンからは現在タブを新ウィンドウに移動するのみに改変
    - 右クリックメニューから同上機能を実行
    原作者様に感謝いたします。ありがとうございましたm(_ _)m

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ combine and split tabs of chrome.
 ## LICENSE
     * MIT License(http://opensource.org/licenses/mit-license.php)
     * Copyright (c) 2013 takc923
+
+## 2020/1/12 sato1043改変版追記
+- 複数タブのcombine/splitを無効化
+- 機能拡張メニューのボタンからは現在タブを新ウィンドウに移動するのみに改変
+- 右クリックメニューから同上機能を実行
+原作者様に感謝いたします。ありがとうございましたm(_ _)m
--- background.js
+++ background.js
@@ -13,19 +13,19 @@ chrome.windows.onRemoved.addListener(function(removedWinId){
     });
 });
 
-chrome.browserAction.onClicked.addListener(function(){
-    chrome.tabs.query({highlighted: true, currentWindow: true}, function(tabs){
-        tabs.length == 1 ? combine() : split(tabs);
-    });
-});
+// chrome.browserAction.onClicked.addListener(function(){
+//     chrome.tabs.query({highlighted: true, currentWindow: true}, function(tabs){
+//         tabs.length == 1 ? combine() : split(tabs);
+//     });
+// });
 
-function combine() {
-    if (winHistory.some(function(id){return id == -1;})) return;
-    chrome.windows.get(winHistory[0], {populate: true}, function(lastWin){
-        var tabIds = lastWin.tabs.map(function(tab){return tab.id;});
-        chrome.tabs.move(tabIds, {windowId: winHistory[1], index: -1});
-    });
-}
+// function combine() {
+//     if (winHistory.some(function(id){return id == -1;})) return;
+//     chrome.windows.get(winHistory[0], {populate: true}, function(lastWin){
+//         var tabIds = lastWin.tabs.map(function(tab){return tab.id;});
+//         chrome.tabs.move(tabIds, {windowId: winHistory[1], index: -1});
+//     });
+// }
 
 function split(tabs){
     var tabIds = tabs.map(function(tab){return tab.id;});
@@ -34,3 +34,13 @@ function split(tabs){
         chrome.tabs.move(tabIds, {windowId: win.id, index: -1});
     });
 }
+
+function _handleSplit() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        split(tabs);
+    });
+}
+
+chrome.browserAction.onClicked.addListener(_handleSplit);
+chrome.contextMenus.create({ title: "このタブを別ウィンドウに移動", onclick: _handleSplit });
+
